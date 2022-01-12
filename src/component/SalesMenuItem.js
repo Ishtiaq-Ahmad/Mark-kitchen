39c283d4f9aec0c19c068ccaf3427ca2672e1778
ishtiaq <ishtiaqdics@gmail.com>
2022-01-12 14:58:04 +0500

    download button done

--- src/component/SalesMenuItem.js
+++ src/component/SalesMenuItem.js
@@ -1,29 +1,22 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
-import ChangeHistoryIcon from '@mui/icons-material/ChangeHistory';
+import * as React from "react";
+import PropTypes from "prop-types";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ChangeHistoryIcon from "@mui/icons-material/ChangeHistory";
+import DownloadIcon from '@mui/icons-material/Download';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 function createData(name, calories, fat, carbs, protein) {
   return {
@@ -36,36 +29,107 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const downicon = {
-    transform: 'scale(.8)'
-}
+  transform: "scale(.8)",
+};
 
 const doown_sale = (
-    <div style={{display:'flex', color:'red',alignItems:"center", justifyContent:"flex-end" }}> 
-        <ArrowDownwardIcon style={downicon}/> 
-      <strong >55%</strong>
-    </div>
-
-)
+  <div
+    style={{
+      display: "flex",
+      color: "red",
+      alignItems: "center",
+      justifyContent: "flex-end",
+    }}
+  >
+    <ArrowDownwardIcon style={downicon} />
+    <strong>55%</strong>
+  </div>
+);
 const up_sale = (
-    <div style={{display:'flex', color:'green',alignItems:"center", justifyContent:"flex-end" }}> 
-        <ArrowUpwardIcon style={downicon}/> 
-      <strong >8%</strong>
-    </div>
-
-)
-
+  <div
+    style={{
+      display: "flex",
+      color: "green",
+      alignItems: "center",
+      justifyContent: "flex-end",
+    }}
+  >
+    <ArrowUpwardIcon style={downicon} />
+    <strong>8%</strong>
+  </div>
+);
 
 const rows = [
-  createData(<strong>1</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, doown_sale, 56),
-  createData(<strong>2</strong>, 'Double Shouble Deal 1', <strong>27,944.00</strong>, doown_sale, 34),
-  createData(<strong>3</strong>, 'French Fries', <strong>27,944.00</strong>, doown_sale, 20),
-  createData(<strong>4</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, doown_sale, 14),
-  createData(<strong>5</strong>, 'Kentucky burger', <strong>27,944.00</strong>, up_sale, 13),
-  createData(<strong>6</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, doown_sale, 52),
-  createData(<strong>7</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, doown_sale, 23),
-  createData(<strong>8</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, doown_sale, 2),
-  createData(<strong>9</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, doown_sale, 45),
-  createData(<strong>10</strong>, 'Americano Deal 1', <strong>27,944.00</strong>, up_sale, 32),
+  createData(
+    <strong>1</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    56
+  ),
+  createData(
+    <strong>2</strong>,
+    "Double Shouble Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    34
+  ),
+  createData(
+    <strong>3</strong>,
+    "French Fries",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    20
+  ),
+  createData(
+    <strong>4</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    14
+  ),
+  createData(
+    <strong>5</strong>,
+    "Kentucky burger",
+    <strong>Rs 27,944.00</strong>,
+    up_sale,
+    13
+  ),
+  createData(
+    <strong>6</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    52
+  ),
+  createData(
+    <strong>7</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    23
+  ),
+  createData(
+    <strong>8</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    2
+  ),
+  createData(
+    <strong>9</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    doown_sale,
+    45
+  ),
+  createData(
+    <strong>10</strong>,
+    "Americano Deal 1",
+    <strong>Rs 27,944.00</strong>,
+    up_sale,
+    32
+  ),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -79,13 +143,11 @@ function descendingComparator(a, b, orderBy) {
 }
 
 function getComparator(order, orderBy) {
-  return order === 'desc'
+  return order === "desc"
     ? (a, b) => descendingComparator(a, b, orderBy)
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
 function stableSort(array, comparator) {
   const stabilizedThis = array.map((el, index) => [el, index]);
   stabilizedThis.sort((a, b) => {
@@ -99,81 +161,63 @@ function stableSort(array, comparator) {
 }
 
 const tricon = {
-    transform: 'scale(.9)'
-}
+  transform: "scale(.9)",
+};
 
 const headCells = [
   {
-    id: 'name',
+    id: "name",
     numeric: false,
     disablePadding: true,
-    label: 'Leaderboard',
+    label: "Leaderboard",
   },
   {
-    id: 'calories',
-    // numeric: true,
-    // disablePadding: false,
-    label: 'Menu item',
+    id: "calories",
+    label: "Menu item",
   },
   {
-    id: 'fat',
+    id: "fat",
     numeric: true,
     disablePadding: false,
-    label: 'Sales',
+    label: "Sales",
   },
   {
-    id: 'carbs',
+    id: "carbs",
     numeric: true,
     disablePadding: false,
-    label: <ChangeHistoryIcon style={tricon}/>,
+    label: <ChangeHistoryIcon style={tricon} />,
   },
   {
-    id: 'protein',
+    id: "protein",
     numeric: true,
     disablePadding: false,
-    label: 'Item Sold',
+    label: "Item Sold",
   },
 ];
 
 function EnhancedTableHead(props) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
-    props;
+  const { order, orderBy, onRequestSort } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
 
   return (
-    <TableHead sx={{ borderBottom:'2px solid #fbbe36'}}>
+    <TableHead sx={{ borderBottom: "2px solid #fbbe36" }}>
       <TableRow>
-        <TableCell padding="checkbox">
-          {/* <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          /> */}
-        </TableCell>
+        <TableCell padding="checkbox"></TableCell>
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'normal'}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
             sortDirection={orderBy === headCell.id ? order : false}
           >
             <TableSortLabel
               active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
+              direction={orderBy === headCell.id ? order : "asc"}
               onClick={createSortHandler(headCell.id)}
             >
               {headCell.label}
-              {/* {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null} */}
             </TableSortLabel>
           </TableCell>
         ))}
@@ -183,112 +227,23 @@ function EnhancedTableHead(props) {
 }
 
 EnhancedTableHead.propTypes = {
-//   numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-//   onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
 };
 
-// const EnhancedTableToolbar = (props) => {
-//   const { numSelected } = props;
-
-//   return (
-//     <Toolbar
-//       sx={{
-//         pl: { sm: 2 },
-//         pr: { xs: 1, sm: 1 },
-//         ...(numSelected > 0 && {
-//           bgcolor: (theme) =>
-//             alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-//         }),
-//       }}
-//     >
-//     Hello Table
-//       {numSelected > 0 ? (
-//         <Typography
-//           sx={{ flex: '1 1 100%' }}
-//           color="inherit"
-//           variant="subtitle1"
-//           component="div"
-//         >
-//           {numSelected} selected
-//         </Typography>
-//       ) : (
-//         <Typography
-//           sx={{ flex: '1 1 100%' }}
-//           variant="h6"
-//           id="tableTitle"
-//           component="div"
-//         >
-//           Nutrition
-//         </Typography>
-//       )}
-
-//       {numSelected > 0 ? (
-//         <Tooltip title="Delete">
-//           <IconButton>
-//             <DeleteIcon />
-//           </IconButton>
-//         </Tooltip>
-//       ) : (
-//         <Tooltip title="Filter list">
-//           <IconButton>
-//             <FilterListIcon />
-//           </IconButton>
-//         </Tooltip>
-//       )}
-//     </Toolbar>
-//   );
-// };
-
-// EnhancedTableToolbar.propTypes = {
-//   numSelected: PropTypes.number.isRequired,
-// };
-
 export default function SalesMenuItem() {
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
-//   const [selected, setSelected] = React.useState([]);
+  const [order, setOrder] = React.useState("asc");
+  const [orderBy, setOrderBy] = React.useState("calories");
   const [page, setPage] = React.useState(0);
-//   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
-
-//   const handleSelectAllClick = (event) => {
-//     if (event.target.checked) {
-//       const newSelecteds = rows.map((n) => n.name);
-//       setSelected(newSelecteds);
-//       return;
-//     }
-//     setSelected([]);
-//   };
-
-//   const handleClick = (event, name) => {
-//     const selectedIndex = selected.indexOf(name);
-//     let newSelected = [];
-
-//     if (selectedIndex === -1) {
-//       newSelected = newSelected.concat(selected, name);
-//     } else if (selectedIndex === 0) {
-//       newSelected = newSelected.concat(selected.slice(1));
-//     } else if (selectedIndex === selected.length - 1) {
-//       newSelected = newSelected.concat(selected.slice(0, -1));
-//     } else if (selectedIndex > 0) {
-//       newSelected = newSelected.concat(
-//         selected.slice(0, selectedIndex),
-//         selected.slice(selectedIndex + 1),
-//       );
-//     }
-
-//     setSelected(newSelected);
-//   };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -299,83 +254,72 @@ export default function SalesMenuItem() {
     setPage(0);
   };
 
-//   const handleChangeDense = (event) => {
-//     setDense(event.target.checked);
-//   };
-
-//   const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-//   const emptyRows =
-//     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
   return (
-    <Box sx={{ width: '100%' }}>
-      <Paper sx={{ width: '100%', mb: 2 }}>
-        {/* <EnhancedTableToolbar /> */}
-         <Typography variant="h6" component="div" gutterBottom style={{padding:"10px 30px 0px 30px"}}>
-        Sales by Menu Item
-      </Typography>
+    <Box sx={{ width: "100%" }}>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <Typography
+          variant="h6"
+          component="div"
+          gutterBottom
+          style={{ padding: "10px 30px 0px 30px" }}
+        >
+          Sales by Menu Item
+        </Typography>
+          <Grid container spacing={0}>
+        <Grid item xs={9} md={10}>
+         <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          style={{ padding: "10px 30px 0px 30px" }}
+        >
+         Ranking of which menu items are the most and least popular. Use this to see whih of your menu items are trending up or down over time. Note: this report does not include menu item additions like extra toppings.
+        </Typography>
+        </Grid>
+        <Grid item xs={3} md={2} style={{display:'flex', alignItems:"center"}}>
+          <Button  endIcon={<DownloadIcon />}>Download Report</Button>
+        </Grid>
+       
+      </Grid>
+        {/* <div style={{display:'flex', justifyContent:"space-between"}}>
+         <Typography
+          variant="body1"
+          component="div"
+          gutterBottom
+          style={{ padding: "10px 30px 0px 30px" }}
+        >
+         Ranking of which menu items are the most and least popular. Use this to see whih of your menu items are trending up or down over time. Note: this report does not include menu item additions like extra toppings.
+        </Typography>
+        <Button endIcon={<DownloadIcon />}>Download Report</Button>
+        </div> */}
+       
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
             aria-labelledby="tableTitle"
-            size= 'medium'
+            size="medium"
           >
             <EnhancedTableHead
-            //   numSelected={selected.length}
-            //   order={order}
               orderBy={orderBy}
-            //   onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
-            //   rowCount={rows.length}
             />
             <TableBody>
-            
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  {/* const isItemSelected = isSelected(row.name); */}
-                  {/* const labelId = `enhanced-table-checkbox-${index}`; */}
-
                   return (
-                    <TableRow
-                      hover
-                    //   onClick={(event) => handleClick(event, row.name)}
-                    //   role="checkbox"
-                    //   aria-checked={isItemSelected}
-                    // s  tabIndex={-1}
-                    //   key={row.name}
-                    //   selected={isItemSelected}
-                    >
-                      <TableCell>
-                        {/* <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                        /> */}
-                      </TableCell>
-                      <TableCell component="th"  scope="row" padding="none">
+                    <TableRow hover>
+                      <TableCell></TableCell>
+                      <TableCell component="th" scope="row" padding="none">
                         {row.name}
                       </TableCell>
-                      <TableCell >{row.calories}</TableCell>
+                      <TableCell>{row.calories}</TableCell>
                       <TableCell align="right">{row.fat}</TableCell>
                       <TableCell align="right">{row.carbs}</TableCell>
                       <TableCell align="right">{row.protein}</TableCell>
                     </TableRow>
                   );
                 })}
-              {/* {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )} */}
             </TableBody>
           </Table>
         </TableContainer>
@@ -389,10 +333,6 @@ export default function SalesMenuItem() {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </Paper>
-      {/* <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      /> */}
     </Box>
   );
 }
